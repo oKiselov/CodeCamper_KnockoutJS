9734a1d24237904b1b5b1600ad36c78c47ebfb0c
Alex Kiselov <dp.prk.kiselov@gmail.com>
2018-10-01 01:08:48 +0300

    Initial commit for KnockoutMedia

---
+++ CodeCamper.Web/CodeCamper.Model/Session.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace CodeCamper.Model
+{
+    public class Session : SessionBrief
+    {
+        public string Description { get; set; }
+
+        public Person Speaker { get; set; }
+
+        public virtual Track Track { get; set; }
+
+
+        public virtual ICollection<Attendance> AttendanceList { get; set; }
+    }
+}
---
+++ CodeCamper.Web/CodeCamper.Model/Track.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CodeCamper.Model
+{
+    public class Track
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
