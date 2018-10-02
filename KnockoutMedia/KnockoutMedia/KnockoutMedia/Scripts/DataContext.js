31444c46cce686bf0ea7033525d4d25757e55685
okiselov <kiselov.oleksandr@apriorit.com>
2018-10-02 13:58:10 +0300

    Sections 1 - 2 in Packt video course. Initial data binding attrs.

--- KnockoutMedia/KnockoutMedia/KnockoutMedia/Scripts/DataContext.js
+++ KnockoutMedia/KnockoutMedia/KnockoutMedia/Scripts/DataContext.js
@@ -1,21 +1,15 @@
 ﻿var app = window.app || {};
-app.DataContext = (function () {
+app.DataContext = (function ($) {
     'use strict';
     var me = {
         getCatalog: getCatalog
     };
 
     function getCatalog(callback) {
-        if (typeof callback === 'function') {
-            var catalog = localStorage["catalog"];
-            if (catalog) {
-                callback(JSON.parse(catalog));
-            } else {
-                $.getJSON('/Data/Catalog.json', function (data) {
-                    localStorage["catalog"] = JSON.stringify(data.Catalog);
-                    callback(data.Catalog);
-                });
-            }
+        if ($.isFunction(callback)) {
+            $.getJSON('/Data/Catalog.json', function (data) {
+                callback(data.Catalog);
+            });
         }
     }
 
