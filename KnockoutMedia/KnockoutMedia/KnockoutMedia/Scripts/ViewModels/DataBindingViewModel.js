a976641e756082ee7efcfbc26ed61f3d2eae9741
Alex Kiselov <dp.prk.kiselov@gmail.com>
2018-10-04 01:09:56 +0300

    008

---
+++ KnockoutMedia/KnockoutMedia/KnockoutMedia/Scripts/ViewModels/DataBindingViewModel.js
@@ -0,0 +1,22 @@
+﻿var app = window.app || {};
+app.DefaultViewModel = (function (ko) {
+    'use strict';
+    var me = {
+        message: ko.observable(''),
+        colors: ["Red", "Greed", "Blue", "Yellow", "Black", "White"],
+        loadMessage: loadMessage
+    };
+
+    function _init() {
+        me.message.subscribe(function(v){
+
+        });
+    }
+
+    function loadMessage() {
+        me.message("My favourite is: " + me.colors[Math.floor(Math.random() * mew.colors.length)].toLowerCase());
+    }
+
+    _init();
+    return me;
+}(ko));
