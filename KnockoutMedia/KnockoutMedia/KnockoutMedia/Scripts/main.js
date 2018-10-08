34228a6e8bf4e9144fe39fb3fbef1b7806f3933d
Alex Kiselov <dp.prk.kiselov@gmail.com>
2018-10-09 01:35:35 +0300

    First touch to Require.JS

---
+++ KnockoutMedia/KnockoutMedia/KnockoutMedia/Scripts/main.js
@@ -0,0 +1,5 @@
+﻿require(['knockout-3.4.2', 'ViewModels/RequireViewModel'], function (ko, viewModel) {
+    'use strict';
+    ko.components.register('color-list', { require: 'Components/ColorListRequire' });
+    ko.applyBindings(new viewModel());
+});
